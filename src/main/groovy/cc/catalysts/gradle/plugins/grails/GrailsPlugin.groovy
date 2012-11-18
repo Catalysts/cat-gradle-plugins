3527143472e9f43b464b54f3672a19d77b63e4fe
Andreas Gerstmayr <andreas@andreasgerstmayr.at>
2012-11-18 23:32:18 +0100

    cat-grails: fix sonar detection

--- src/main/groovy/cc/catalysts/gradle/plugins/grails/GrailsPlugin.groovy
+++ src/main/groovy/cc/catalysts/gradle/plugins/grails/GrailsPlugin.groovy
@@ -12,7 +12,7 @@ import org.gradle.api.plugins.sonar.SonarPlugin
 class GrailsPlugin implements Plugin<Project> {
     Project project
 
-    void applyGroovy() {
+    private void applyGroovy() {
         project.apply plugin: 'groovy'
 
         // source set definitions for the sonar plugin
@@ -37,44 +37,64 @@ class GrailsPlugin implements Plugin<Project> {
         }
     }
 
-    void applyGrailsWrapper() {
+    private void applyGrailsWrapper() {
         project.apply plugin: GradleGrailsWrapperPlugin
     }
 
-    void addTasks() {
-        // clean depends on grails-clean
-        project.tasks.clean.dependsOn(project.tasks.'grails-clean')
-
+    private  Task addTestTask() {
         // map test task to grails-test-app
         Task testTask = project.task('test',
                 group: 'cat-grails',
                 description: 'Tests the project',
                 overwrite: true)
-        testTask.convention.testResultsDir = project.file("target/test-reports")
-        testTask.dependsOn(project.tasks.'grails-test-app')
+        testTask.convention.testResultsDir = project.file('target/test-reports')
+        return testTask
+    }
 
+    private Task addBuildTask() {
         // empty build task, depends on test (and if it's a grails app) on war
         Task buildTask = project.task('build',
                 group: 'cat-grails',
                 description: 'Tests the project and (if it is an application) builds the war archive',
                 overwrite: true)
+        return buildTask
+    }
+
+    private Task addWarTask() {
+        Task warTask = project.task('war',
+                group: 'cat-grails',
+                description: 'Creates a war archive of the grails application')
+        return warTask
+    }
+
+    private void addTasks() {
+        // clean depends on grails-clean
+        project.tasks.clean.dependsOn(project.tasks.'grails-clean')
+
+        Task testTask = addTestTask()
+        testTask.dependsOn(project.tasks.'grails-test-app')
+
+        Task buildTask = addBuildTask()
         buildTask.dependsOn(testTask)
 
         if (GrailsUtils.isGrailsApplication(project)) {
-            Task warTask = project.task('war',
-                    group: 'cat-grails',
-                    description: 'Creates a war archive of the grails application')
+            Task warTask = addWarTask()
             warTask.dependsOn(project.tasks.'grails-war')
-
             buildTask.dependsOn(warTask)
         }
     }
 
-    void apply(Project project) {
+    public void apply(Project project) {
         this.project = project
 
-        if (project.plugins.hasPlugin(SonarPlugin)) {
-            project.sonar.project.language = 'grvy'
+        // in case there is no rootProject, rootProject simply points to the current project
+        if (project.plugins.hasPlugin(SonarPlugin) || project.rootProject.plugins.hasPlugin(SonarPlugin)) {
+            project.sonar.project {
+                language = 'grvy'
+                testReportPath = project.file('target/test-reports')
+                coberturaReportPath = project.file('target/test-reports/cobertura/coverage.xml')
+                dynamicAnalysis = 'reuseReports'
+            }
         }
 
         applyGroovy()
