be5e1251a6a6569660a1c871beab1a6a8de2ec12
Lucas Binter <lucas.binter@catalysts.cc>
2014-07-08 15:28:57 +0200

    updated querydsl plugin to use TCLogger

--- src/main/groovy/cc/catalysts/gradle/plugins/querydsl/CleanQueryDslTask.groovy
+++ src/main/groovy/cc/catalysts/gradle/plugins/querydsl/CleanQueryDslTask.groovy
@@ -1,5 +1,6 @@
 package cc.catalysts.gradle.plugins.querydsl
 
+import cc.catalysts.gradle.utils.TCLogger
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -7,13 +8,15 @@ import org.gradle.api.tasks.TaskAction
  * @author Catalysts GmbH, www.catalysts.cc
  */
 class CleanQueryDslTask extends DefaultTask {
+    private TCLogger log = new TCLogger(project, logger)
+
     @TaskAction
     def cleanQueryDslTargetDir() {
         File f = new File(project.projectDir.absolutePath, project.querydsl.destinationDir);
-        if (f.exists()){
-            println "     cat-querydsl: Deleting '" + f.getPath() + "'"
-            if (!f.deleteDir()){
-                println "          Error in deleting directory"
+        if (f.exists()) {
+            log.lifecycle "Deleting '${f.getPath()}'"
+            if (!f.deleteDir()) {
+                log.failure "Could not delete directory '${f.getPath()}'", true
             }
         }
     }
--- src/main/groovy/cc/catalysts/gradle/plugins/querydsl/QueryDslOutputDirTask.groovy
+++ src/main/groovy/cc/catalysts/gradle/plugins/querydsl/QueryDslOutputDirTask.groovy
@@ -1,5 +1,6 @@
 package cc.catalysts.gradle.plugins.querydsl
 
+import cc.catalysts.gradle.utils.TCLogger
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -7,13 +8,15 @@ import org.gradle.api.tasks.TaskAction
  * @author Catalysts GmbH, www.catalysts.cc
  */
 class QueryDslOutputDirTask extends DefaultTask {
+    private TCLogger log = new TCLogger(project, logger)
+
     @TaskAction
     def createQueryDslOutputDir() {
         File f = new File(project.projectDir.absolutePath, project.querydsl.destinationDir);
-        if (!f.exists()){
-            println "     cat-querydsl: Creating '" + f.getPath() + "'"
-           if (!f.mkdirs()){
-                println "          Error in creating directory"
+        if (!f.exists()) {
+            log.lifecycle "Creating '${f.getPath()}'"
+            if (!f.mkdirs()) {
+                log.failure "Could not create directory '${f.getPath()}'", true
             }
         }
     }
