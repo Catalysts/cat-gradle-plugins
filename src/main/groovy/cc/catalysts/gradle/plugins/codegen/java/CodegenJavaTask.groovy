ce75af0f5e550b38370301a15f265ad195fd24f9
Lucas Binter <lucas.binter@catalysts.cc>
2014-07-08 14:56:58 +0200

    updated Codegen to use TCLogger

--- src/main/groovy/cc/catalysts/gradle/plugins/codegen/java/CodegenJavaTask.groovy
+++ src/main/groovy/cc/catalysts/gradle/plugins/codegen/java/CodegenJavaTask.groovy
@@ -1,5 +1,6 @@
 package cc.catalysts.gradle.plugins.codegen.java
 
+import cc.catalysts.gradle.utils.TCLogger
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import java.text.SimpleDateFormat
@@ -8,6 +9,7 @@ import java.text.SimpleDateFormat
  * @author Catalysts GmbH, www.catalysts.cc
  */
 class CodegenJavaTask extends DefaultTask {
+    private TCLogger log = new TCLogger(project, logger)
 	
 	CodegenJavaTask() {
 		project.tasks.codegen.dependsOn(this)
@@ -16,19 +18,20 @@ class CodegenJavaTask extends DefaultTask {
 	
 	@TaskAction
 	def build() {
-		def pName = project.codegenjava.packageName
+		def pName = project.codegenjava.packageName as String
 		if(pName != null) {
 			Calendar calendar = Calendar.getInstance()
 			SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm")
 			def date = dateFormat.format(calendar.getTime())
 			def version = project.version
-			project.codegenjava.destDirs.each {
+            (project.codegenjava.destDirs as List<String>).each {
 				File destDir = new File(it)
 				if(destDir.exists()) {
 					destDir.deleteDir()
 				}
 				new File(it + "/" + pName.replace('.', '/')).mkdirs()
 				def f = new File(it + "/" + pName.replace('.', '/') + '/Build.java')
+                log.lifecycle "Writing new file '${f.getAbsolutePath()}'"
 				def w = f.newWriter()
 				
 				w << 'package ' << pName << ';\r\n'
