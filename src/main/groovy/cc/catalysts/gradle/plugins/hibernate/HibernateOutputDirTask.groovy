5e05b66e1d269d40e25b7c4298f3988023f5ef76
Lucas Binter <lucas.binter@catalysts.cc>
2014-07-08 15:05:22 +0200

    updated hibernate plugin to use TCLogger

--- src/main/groovy/cc/catalysts/gradle/plugins/hibernate/HibernateOutputDirTask.groovy
+++ src/main/groovy/cc/catalysts/gradle/plugins/hibernate/HibernateOutputDirTask.groovy
@@ -1,5 +1,6 @@
 package cc.catalysts.gradle.plugins.hibernate
 
+import cc.catalysts.gradle.utils.TCLogger
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -7,13 +8,15 @@ import org.gradle.api.tasks.TaskAction
  * @author Catalysts GmbH, www.catalysts.cc
  */
 class HibernateOutputDirTask extends DefaultTask {
+    private TCLogger log = new TCLogger(project, logger)
+
     @TaskAction
     def createHibernateOutputDir() {
-        File f = new File(project.projectDir.absolutePath, project.hibernate.destinationDir);
-        if (!f.exists()){
-            println "     cat-hibernate: Creating '" + f.getPath() + "'"
-           if (!f.mkdirs()){
-                println "          Error in creating directory"
+        File f = new File(project.projectDir.absolutePath, project.hibernate.destinationDir as String);
+        if (!f.exists()) {
+            log.lifecycle "Creating '${f.getPath()}'"
+            if (!f.mkdirs()) {
+                log.failure "Could not create directory '${f.getPath()}'", true
             }
         }
     }
