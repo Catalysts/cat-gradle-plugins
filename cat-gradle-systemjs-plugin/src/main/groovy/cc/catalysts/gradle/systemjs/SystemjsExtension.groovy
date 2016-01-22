d25766cbc3fda746546c54bd29fc4f39a0d2029f
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2016-01-22 14:17:06 +0100

    move generated files to build dir

--- cat-gradle-systemjs-plugin/src/main/groovy/cc/catalysts/gradle/systemjs/SystemjsExtension.groovy
+++ cat-gradle-systemjs-plugin/src/main/groovy/cc/catalysts/gradle/systemjs/SystemjsExtension.groovy
@@ -15,7 +15,7 @@ class SystemjsExtension {
         srcDir = new File(project.projectDir, 'src/main/resources')
 //        destinationDir = new File(project.buildDir, 'generated-resources/systemjs-bundle')
         bundlePath = "META-INF/resources/webjars/${project.name}/${project.rootProject.version}"
-        destinationDir = new File(project.projectDir, "target/generated-resources/cat-systemjs")
+        destinationDir = new File(project.buildDir, "generated-resources/cat-systemjs")
     }
 
     File getBundleLocation() {
