1c5b5e74fa24bb8a16c1bef2a03be2a62f72f573
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2016-01-28 17:06:23 +0100

    make less install run always again (due to some yet to determine issue)

--- cat-gradle-less-plugin/src/main/groovy/cc/catalysts/gradle/less/task/InstallLess.groovy
+++ cat-gradle-less-plugin/src/main/groovy/cc/catalysts/gradle/less/task/InstallLess.groovy
@@ -1,12 +1,7 @@
 package cc.catalysts.gradle.less.task
 
 import cc.catalysts.gradle.less.LessExtension
-import com.moowork.gradle.node.NodeExtension
 import com.moowork.gradle.node.task.NpmTask
-import org.gradle.execution.commandline.TaskConfigurationException
-
-import java.nio.file.Files
-
 /**
  * @author Thomas Scheinecker, Catalysts GmbH
  */
@@ -14,34 +9,8 @@ class InstallLess extends NpmTask {
     public InstallLess() {
         this.group = 'Cat-Boot LESS'
         this.description = 'Prepares the less compiler'
-
-
-        project.afterEvaluate({
-            File nodeModulesDir = LessExtension.get(project).nodeModulesDir
-
-            if (!nodeModulesDir.exists() && !nodeModulesDir.mkdirs()) {
-                throw new TaskConfigurationException(path, "Couldn't create ${nodeModulesDir}", null)
-            }
-
-            File projectPackageJson = new File(nodeModulesDir, 'package.json')
-
-            InputStream packageJson = InstallLess.classLoader.getResourceAsStream('cat-gradle/less/package.json')
-            List<String> lines = packageJson.readLines('utf-8')
-            Files.write(projectPackageJson.toPath(), lines).toFile().exists()
-
-            setWorkingDir(nodeModulesDir)
-            setNpmCommand('install')
-
-            inputs.file(projectPackageJson)
-            outputs.dir(new File(nodeModulesDir, 'node_modules'))
-        })
-    }
-
-    @Override
-    void exec() {
-        println "node.download ${NodeExtension.get( this.project ).download}"
-        println "node.nodeModulesDir ${NodeExtension.get( this.project ).nodeModulesDir}"
-        println "runner.workdir ${runner.workingDir}"
-        super.exec()
+        File nodeModulesDir = LessExtension.get(project).nodeModulesDir
+        setWorkingDir(nodeModulesDir)
+        setNpmCommand('install', 'less', 'less-plugin-autoprefix',  'less-plugin-clean-css')
     }
 }
